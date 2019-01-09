bd7b20c6d54dac218fc52b2c44b86b40ca2ea97b
Daniel Törbacka <daniel.torbacka@gmail.com>
2019-01-09 11:54:20 +0100

    First layout of smart-hub

---
+++ src/main/java/se/taco/smarthub/SmartHubApplication.java
@@ -0,0 +1,13 @@
+package se.taco.smarthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHubApplication.class, args);
+    }
+}
+
---
+++ src/main/java/se/taco/smarthub/controller/TemperatureController.java
@@ -0,0 +1,25 @@
+package se.taco.smarthub.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TemperatureController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TemperatureController.class);
+
+
+    @PostMapping(value = "/temperature", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity cloudEvent(@RequestBody String body) {
+
+        logger.info("body: {}", body);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+}
